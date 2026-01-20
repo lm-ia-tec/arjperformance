feb0a3757558aed21a014dd4016fb571e110828e
Lucas Marques <lucasmarques.cont@gmail.com>
2026-01-20 17:20:02 -0300

    Update transformacoes.py

--- Importação/services/transformacoes.py
+++ Importação/services/transformacoes.py
@@ -1,5 +1,1 @@
-def ajustar_layout_fortes(df, estabelecimento_padrao, centro_padrao):
-    df["Estabelecimento"] = df.get("Estabelecimento", estabelecimento_padrao)
-    df["CentroCusto"] = df.get("CentroCusto", centro_padrao)
 
-    return df
