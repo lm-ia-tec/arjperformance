649da8c9de6b5de567c83afefd95af9a4062bddf
Lucas Marques <lucasmarques.cont@gmail.com>
2026-01-20 11:12:21 -0300

    Update sidebar.py

--- Importação/ui/sidebar.py
+++ Importação/ui/sidebar.py
@@ -12,24 +12,6 @@ def render_sidebar():
         """
     )
 
-    # Rodapé da barra lateral
-    st.sidebar.markdown(
-        """
-        <div style="
-            position: absolute;
-            bottom: 10px;
-            width: 90%;
-            text-align: center;
-            font-size: 0.9em;
-            color: gray;
-        ">
-            Versão 1.0<br>
-            Desenvolvido por Lucas Marques
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
 
 
 
